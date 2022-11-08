4e29fcb56a29a78e1cc1035fc1627c4dae6521e6
ALUMNO <ALUMNO@192.168.1.122>
2022-11-08 08:37:59 -0600

    crud

---
+++ Trabajo/src/Modelo/Alumno.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+public class Alumno {
+	int id;
+	String nombre;
+	int grupo;
+	String correo;
+	String semestre;
+
+}
---
+++ Trabajo/src/Vista/vAlumno.java
@@ -0,0 +1,41 @@
+package Vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class vAlumno extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vAlumno frame = new vAlumno();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public vAlumno() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
